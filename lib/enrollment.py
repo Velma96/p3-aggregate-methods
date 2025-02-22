e39d46015c66ac4bbd988e397b4a17e5ea4bba6a
phoebe velma awuor <awuorphoebi@gmail.com>
2025-02-22 23:15:38 +0300

    complete

--- lib/enrollment.py
+++ lib/enrollment.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+
 class Student:
     def __init__(self, name):
         self.name = name
@@ -15,9 +16,11 @@ class Student:
     def get_enrollments(self):
         return self._enrollments.copy()
 
+    def course_count(self):
+        return len(self._enrollments)
+
 class Course:
     def __init__(self, title):
-
         self.title = title
         self._enrollments = []
 
@@ -30,6 +33,8 @@ class Course:
     def get_enrollments(self):
         return self._enrollments.copy()
 
+    def student_count(self):
+        return len(self._enrollments)
 
 class Enrollment:
     all = []
@@ -45,3 +50,11 @@ class Enrollment:
 
     def get_enrollment_date(self):
         return self._enrollment_date
+
+    @classmethod
+    def aggregate_enrollments_per_day(cls):
+        enrollment_count = {}
+        for enrollment in cls.all:
+            date = enrollment.get_enrollment_date().date()
+            enrollment_count[date] = enrollment_count.get(date, 0) + 1
+        return enrollment_count
